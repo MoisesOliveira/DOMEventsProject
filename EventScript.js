cd10e06a2deff029a9c1f87b500375b91085dfcb
moises <moises.santos.oliveira91@gmail.com>
2020-04-29 18:41:00 -0300

    added comments to script code

--- EventScript.js
+++ EventScript.js
@@ -2,10 +2,10 @@ window.addEventListener("load", ()=>{
     const canvas = document.getElementById("pointCanvas");
     const context = canvas.getContext("2d");
     const colorInput = document.getElementById("colorInput");
-
+    //when true, allow the draw function works
     let painting = false;
-    let drawColor = colorInput.value;    
-
+    let drawColor = colorInput.value; 
+    //change the value of painting variable
     function startPosition(e){
         painting = true;
         drawLines(e);
@@ -13,6 +13,7 @@ window.addEventListener("load", ()=>{
     function endPosition(){
         painting = false;
     }
+    // drawing funcyion on canvas element
     function drawLines(e){
         // x and y coordinates related to mouse events
         var coord = {
@@ -29,6 +30,7 @@ window.addEventListener("load", ()=>{
         context.beginPath();
         context.moveTo(coord.x,coord.y);
     }
+    //mouse events 
     canvas.addEventListener("mousedown",startPosition);
     canvas.addEventListener("mouseup",endPosition);
     canvas.addEventListener("mousemove",drawLines);
